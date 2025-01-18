205e5c2202c5c69f945d2f10c0f423dcf30084e1
EC2 Default User <ec2-user@ip-172-31-3-71.us-east-2.compute.internal>
2025-01-18 01:01:14 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const { newweight } = require('./newweight')
+require('dotenv').config
+
+const express = require('express')
+const { Client } = require('@notionhq/client');
+
+
+const app = express();
+app.use(espress.json())
+
+
+const PORT = 3000
+
+app.post('/', (req, res) => {
+    console.log("Hit")
+    res.send('Hellow World');
+})
+
+
+app.get('/', (req, res) => {
+    console.log("Get was hit at least");
+    res.send('Get was hit')
+})
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+app.use('/neweight')
